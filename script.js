e8cc483f729ec3fdfea3be0615785c2dd362298c
ajoxales <ajoxales@gmail.com>
2023-06-28 21:45:37 +0800

    media queries

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // Get the display element
-const display = document.querySelector('.display');
+const display = document.querySelector(".display");
 
 // Get all the buttons
-const buttons = Array.from(document.querySelectorAll('.buttons button'));
+const buttons = Array.from(document.querySelectorAll(".buttons button"));
 
 // Add event listeners to the buttons
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
     const value = button.dataset.value;
 
-    if (value === 'AC') {
+    if (value === "AC") {
       // Clear the display
-      display.value = '';
-    } else if (value === 'DEL') {
+      display.value = "";
+    } else if (value === "DEL") {
       // Delete the last character
       display.value = display.value.slice(0, -1);
-    } else if (value === '=') {
+    } else if (value === "=") {
       // Evaluate the expression
       try {
         display.value = eval(display.value);
       } catch (error) {
-        display.value = 'Error';
+        display.value = "Error";
       }
     } else {
       // Append the clicked button value to the display
